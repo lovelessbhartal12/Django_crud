8cc7452e3e6df326de6bfd0175ea1a230aa16ef0
Loblesh bhartal <lovelessbhartal12@gmail.com>
2026-02-20 20:56:51 +0545

    create a database for grocer_items

--- grocery/views.py
+++ grocery/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import GroceryItem
+    
 
 # Create your views here.
 def index(request):
+
+    items=GroceryItem.objects.all()
+    context={
+        'items':items
+    }
     return render(request, 'grocery/index.html')
