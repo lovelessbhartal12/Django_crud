65f11bbd8bbd8ebd022b327f9a1908114fd8a3d8
Loblesh bhartal <lovelessbhartal12@gmail.com>
2026-02-21 07:31:09 +0545

    delete component features

--- grocery/views.py
+++ grocery/views.py
@@ -18,3 +18,12 @@ def toggle_item(request, item_id):
     item.save()
     
     return redirect('grocery:index')
+
+
+def delete_item(request , item_id):
+   if request.method=='POST':
+    item=get_object_or_404(GroceryItem, id=item_id)
+    item.delete()
+
+    return redirect('grocery:index')
+   
